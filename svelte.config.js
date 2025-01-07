0f6b1e1ad02806440dd4f505224be7187058ad37
Falk33n <tim.falk00@gmail.com>
2025-01-07 11:05:31 +0100

    chore: setup project

---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+export default {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter(),
+		alias: {
+			'$src/*': 'src/*',
+			'$lib/*': 'src/lib/*'
+		}
+	},
+	include: ['./src/**/*.js', './src/**/*.ts', './src/**/*.svelte'],
+	exclude: ['./node_modules/**', './**']
+};
